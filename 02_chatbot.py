3b8b2b36fdf496a6df385aa0c4b25a4ab9084350
lklab <alexeibatic@gmail.com>
2024-08-13 15:04:28 +0900

    Update 02_chatbot.py

--- 02_chatbot.py
+++ 02_chatbot.py
@@ -11,7 +11,23 @@ os.environ["LANGCHAIN_API_KEY"] = keys['LANGCHAIN_API_KEY']
 
 # setup model
 from langchain_openai import ChatOpenAI
-model = ChatOpenAI(model="gpt-3.5-turbo")
+model = ChatOpenAI(
+    model="gpt-3.5-turbo",
+)
+
+# setup message history
+from langchain_core.chat_history import (
+    BaseChatMessageHistory,
+    InMemoryChatMessageHistory,
+)
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = InMemoryChatMessageHistory()
+    return store[session_id]
 
 def simpleExample() :
     # setup messages
@@ -29,18 +45,6 @@ def simpleExample() :
 
 def messageHistory() :
     from langchain_core.messages import HumanMessage
-    from langchain_core.chat_history import (
-        BaseChatMessageHistory,
-        InMemoryChatMessageHistory,
-    )
-    from langchain_core.runnables.history import RunnableWithMessageHistory
-
-    store = {}
-
-    def get_session_history(session_id: str) -> BaseChatMessageHistory:
-        if session_id not in store:
-            store[session_id] = InMemoryChatMessageHistory()
-        return store[session_id]
 
     with_message_history = RunnableWithMessageHistory(model, get_session_history)
 
@@ -75,5 +79,115 @@ def messageHistory() :
     )
     print("[abc2] " + response.content)
 
+def promptTemplates() :
+    from langchain_core.messages import HumanMessage
+    from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+
+    # setup prompt template
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a helpful assistant. Answer all questions to the best of your ability in {language}.",
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+        ]
+    )
+
+    chain = prompt | model
+
+    # setup MessageHistory
+    with_message_history = RunnableWithMessageHistory(
+        chain,
+        get_session_history,
+        input_messages_key="messages",
+    )
+    config = {"configurable": {"session_id": "abc5"}}
+
+    # send to model
+    response = with_message_history.invoke(
+        {"messages": [HumanMessage(content="hi! I'm todd")], "language": "Korean"},
+        config=config,
+    )
+    print(response.content)
+
+    response = with_message_history.invoke(
+        {"messages": [HumanMessage(content="whats my name?")], "language": "Korean"},
+        config=config,
+    )
+    print(response.content)
+
+def managingHistory() :
+    from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+    from langchain_core.messages import HumanMessage, AIMessage, trim_messages
+
+    messages = [
+        HumanMessage(content="hi! I'm bob"),
+        AIMessage(content="hi!"),
+        HumanMessage(content="I like vanilla ice cream"),
+        AIMessage(content="nice"),
+        HumanMessage(content="whats 2 + 2"),
+        AIMessage(content="4"),
+        HumanMessage(content="thanks"),
+        AIMessage(content="no problem!"),
+        HumanMessage(content="having fun?"),
+        AIMessage(content="yes!"),
+    ]
+
+    trimmer = trim_messages(
+        max_tokens=65,
+        strategy="last",
+        token_counter=model,
+        include_system=True,
+        allow_partial=False,
+        start_on="human",
+    )
+
+    prompt = ChatPromptTemplate.from_messages(
+        [
+            (
+                "system",
+                "You are a helpful assistant. Answer all questions to the best of your ability in {language}.",
+            ),
+            MessagesPlaceholder(variable_name="messages"),
+        ]
+    )
+
+    from operator import itemgetter
+    from langchain_core.runnables import RunnablePassthrough, RunnableLambda
+
+    chain = (
+        RunnablePassthrough.assign(messages=itemgetter("messages") | RunnableLambda(lambda x: messages + x) | trimmer)
+        | prompt
+        | model
+    )
+
+    with_message_history = RunnableWithMessageHistory(
+        chain,
+        get_session_history,
+        input_messages_key="messages",
+    )
+    config = {"configurable": {"session_id": "abc20"}}
+
+    response = with_message_history.invoke(
+        {
+            "messages": [HumanMessage(content="what math problem did i ask")],
+            "language": "English",
+        },
+        config=config,
+    )
+    print(response.content)
+
+    response = with_message_history.invoke(
+        {
+            "messages": [HumanMessage(content="whats my name?")],
+            "language": "English",
+        },
+        config=config,
+    )
+    print(response.content)
+
 # simpleExample()
-messageHistory()
+# messageHistory()
+# promptTemplates()
+managingHistory()
