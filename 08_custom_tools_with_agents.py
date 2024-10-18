ad50c9b84d7221dd2e8c1df24763002b4094b3a6
lklab <alexeibatic@gmail.com>
2024-10-18 17:55:56 +0900

    마피아 게임 사용자의 발언을 분석하는 에이전트 구현

--- 08_custom_tools_with_agents.py
+++ 08_custom_tools_with_agents.py
@@ -17,48 +17,84 @@ model = ChatOpenAI(
 )
 
 ### Define tools ###
-from langchain_core.tools import tool
-from langchain_core.tools import StructuredTool
+from typing import Optional, Type
 
-@tool
-def doSomething(a: int, b: int) -> int:
-    """Do something two numbers."""
-    print(f'doSomething {a}, {b}')
-    return a + b + 1
+from langchain_core.callbacks import (
+    CallbackManagerForToolRun,
+)
+from langchain_core.tools import BaseTool, StructuredTool
+from pydantic import BaseModel, Field
 
-def idontknow() -> str:
-    print(f'idontknow')
-    return 'I don\'t know!!'
+class EstimationInput(BaseModel) :
+    name: str = Field(description="The name of the person whose role the speaker is claiming.")
+    role: str = Field(description="The role of the individual with the specified name that the speaker is claiming.")
 
-idontknowTool = StructuredTool.from_function(
-    func=idontknow,
-    name="idontknow",
-    description="Use this tool when any other available tool.",
+class ClaimInput(BaseModel) :
+    role: str = Field(description="It must be the speaker's role, not someone else's role.")
+    estimations: list[EstimationInput] = Field(description="The roles the speaker is claiming for other people.",)
+
+class ClaimInputWithoutSpeakerRole(BaseModel) :
+    estimations: list[EstimationInput] = Field(description="The roles the speaker is claiming for other people.",)
+
+class ClaimTool(BaseTool):
+    name: str = "ClaimTool"
+    description: str = "Call this tool to analyze the speaker's message. If the speaker claimed their own role, call this tool. For example, a statement like 'My role is citizen' is considered a claim of their own role."
+    args_schema: Type[BaseModel] = ClaimInput
+    return_direct: bool = True
+
+    def _run(
+        self, role: str, estimations: list[EstimationInput], run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        print(f'role={role} estimations={estimations}')
+        return 'completed'
+
+class ClaimToolWithoutSpeakerRole(BaseTool):
+    name: str = "ClaimToolWithoutSpeakerRole"
+    description: str = "Call this tool to analyze the speaker's message. If the speaker did not claim their own role but only asserted the roles of others, call this tool instead of ClaimTool."
+    args_schema: Type[BaseModel] = ClaimInputWithoutSpeakerRole
+    return_direct: bool = True
+
+    def _run(
+        self, estimations: list[EstimationInput], run_manager: Optional[CallbackManagerForToolRun] = None
+    ) -> str:
+        print(f'estimations={estimations}')
+        return 'completed'
+
+def fallback() -> str:
+    print('fallback')
+    return 'fallback'
+
+fallbackTool = StructuredTool.from_function(
+    func=fallback,
+    name="Fallback",
+    description="If the speaker's message is unrelated to the Mafia game, call this tool.",
     return_direct=True,
 )
 
-tools = [doSomething, idontknowTool]
+tools = [ClaimTool(), ClaimToolWithoutSpeakerRole(), fallbackTool]
+
+### setup system message ###
+from langchain_core.messages import SystemMessage
+
+system_message = SystemMessage(content="The speaker's name is charlotte. The following message is a statement made during a game of Mafia. You need to analyze this message to determine what the speaker is claiming and call the appropriate tool. The names should be the closest match from 'oliver,' 'emma,' 'noah,' 'ava,' 'liam,' 'sophia,' 'mason,' 'isabella,' 'james,' 'mia,' 'benjamin,' 'amelia,' 'ethan,' 'harper,' 'lucas,' 'charlotte,' 'henry,' 'evelyn,' 'jack,' 'grace,' 'none.' The roles should be the closest match from 'citizen,' 'police,' 'mafia,' 'doctor' or 'none.' If the names or roles differ significantly from the given strings or are not present in the message, input the string 'none.'")
 
 ### Create the agent ###
 from langgraph.prebuilt import create_react_agent
 
-agent_executor = create_react_agent(model, tools)
+agent_executor = create_react_agent(
+    model, tools, state_modifier=system_message
+)
 
 ### Run the agent ###
-from langchain_core.messages import HumanMessage, SystemMessage
+messages = [
+    'I\'m a doctor. I\'ll reveal the investigation results. Ethan\'s role is a citizen, and Benjamin is the mafia. Let\'s all vote for Benjamin.',
+    'Ehtan\'s role is a citizen, and Benjamin is the mafia. Let\'s all vote for Benjamin.',
+    'My role is the teacher. and Benjamin is an evil person.',
+    'Henry is the police.',
+    'Tom is the police.',
+    'What\'s the weather today?',
+]
 
-response = agent_executor.invoke({"messages": [
-    # SystemMessage(content="Read the following question and use the appropriate tool to answer it. If the question does not correspond to any available tool, simply respond with 'I don't know.'"),
-    HumanMessage(content="Do something 2 and 5?"),
-]})
-for message in response["messages"] :
-    print(repr(message))
-    print('-----')
-
-response = agent_executor.invoke({"messages": [
-    # SystemMessage(content="Read the following question and use the appropriate tool to answer it. If the question does not correspond to any available tool, simply respond with 'I don't know.'"),
-    HumanMessage(content="What is weather today?"),
-]})
-for message in response["messages"] :
-    print(repr(message))
-    print('-----')
+for message in messages :
+    print(f'\n<{message}>')
+    agent_executor.invoke({'messages': [('user', message)]})
